1ccebbf7ff42c3902d31f587a63332f9512c3fa7
Ayush-n25 <97076920+Ayush-n25@users.noreply.github.com>
2023-09-17 22:29:08 +0530

    Delete front directory

--- front/src/components/headerComp.jsx
+++
@@ -1,8 +0,0 @@
-const Header=()=>{
-    return(
-        <header style={{fontSize:'xx-large',marginTop:'5rem',textAlign:'center',fontWeight:'bolder'}}>
-            To-Do app using MERN
-        </header>
-    )
-}
-export default Header;
